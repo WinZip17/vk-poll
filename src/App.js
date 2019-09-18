d1933e27302cdb555364b2e920919a19f06133b3
WinZip <wydo@protonmail.com>
2019-09-18 12:14:19 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import connect from '@vkontakte/vk-connect';
+import {Div, View} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+import Home from './panels/Home';
+import "./App.css"
+
+//личный токен админа группы
+const token = "c9a7f954e750af0454602622a8c92c566a21dac6f73cc6fd17f35e8fb382a074efaaf04a95c57f9e70447";
+
+//id альбома для загрузки фотографий
+const album_id = "266661770";
+
+//id группы с которой работаем
+const group_id = "185650440";
+
+//картинка по умлочанию должна находиться уже в альбоме группы, при открытии фотки данные можно взять со строки адреса в формате как приведенно ниже
+const defaultPhoto = "photo-185650440_457239022";
+
+
+
+
+
+//получение ссылки на загрузку фотки
+const postPhotoUrl = () => {
+	connect.send("VKWebAppCallAPIMethod", {"method": "photos.getUploadServer", "request_id": "photoUrl", "params": {"album_id": album_id, "group_id": group_id,
+			"v":"5.101", "access_token": token}});
+}
+
+const postPhoto = (url, photo) => {
+	debugger
+	let formData = new FormData();
+	formData.append('photo', photo);
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	fetch(proxyurl+url, {
+		method: 'POST',
+		body: formData,
+		headers: {
+			'Access-Control-Allow-Origin': '*'
+		},
+	})
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			if (data) {
+				debugger
+				savePhoto(data.server, data.photos_list, data.hash);
+			} else {
+				// proccess server errors
+			}
+		})
+		.catch(function (error) {
+			debugger
+			// proccess network errors
+		});
+};
+
+const savePhoto = (server, photos_list, hash) => {
+	connect.send("VKWebAppCallAPIMethod", {"method": "photos.save", "request_id": "photoSave", "params": {"album_id": album_id, "group_id": group_id,
+			"server": server, "photos_list": photos_list, "hash": hash, "v":"5.101", "access_token": token}});
+};
+
+const createPoll = () => {
+	let answers = JSON.stringify(["Возможно это я", "Пиши, я ее знаю", "Посмотреть результаты"]);
+	connect.send("VKWebAppCallAPIMethod", {"method": "polls.create", "request_id": "createPoll", "params": {"question": "Заголовок запроса", "is_anonymous": "1",
+			"is_multiple": "0", "owner_id": "-185650440", "add_answers": answers, "v":"5.101", "access_token": token}});
+}
+
+const createMessage = (message,poll, photo) => {
+	//конвертация меседжа
+	let newMessage = JSON.stringify(message);
+
+	let attachments = [photo, poll];
+	//сгенерированный guid
+	let guid = Math.floor(1000000000 + Math.random() * (9000000000 + 1 - 1000000000));
+
+	connect.send("VKWebAppCallAPIMethod", {"method": "wall.post", "request_id": "sendWall", "params": {"owner_id": "-185650440", "from_group": "1",
+			"message": newMessage, "attachments": attachments, "guid": guid, "v":"5.101", "access_token": token}});
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			activePanel: 'home',
+			fetchedUser: null,
+			field1: "",
+			field2: "",
+			field3: "",
+			field4: "",
+			img : null,
+			imageUrl: "",
+			isReady: false,
+			male: true,
+			imgForMessage: defaultPhoto,
+			postSendId: 0
+		};
+	}
+
+	componentDidMount() {
+		connect.subscribe((e) => {
+			switch (e.detail.type) {
+				case 'VKWebAppGetUserInfoResult':
+					this.setState({ fetchedUser: e.detail.data });
+					if (e.detail.data.sex === 1) {
+						this.setState({ male: false });
+					}
+					break;
+				case 'VKWebAppCallAPIMethodResult':
+                    switch (e.detail.data.request_id){
+                        case 'photoUrl':
+                            this.setState({imageUrl: e.detail.data.response.upload_url});
+                            postPhoto(e.detail.data.response.upload_url, this.state.img);
+                            break;
+                        case 'photoSave':
+							this.setState({imgForMessage: `photo${e.detail.data.response[0].owner_id}_${e.detail.data.response[0].id}`});
+							createPoll();
+                            break;
+                        case 'createPoll':
+                        	let poll = `poll${e.detail.data.response.owner_id}_${e.detail.data.response.id}`;
+							let message = `Ищу ${this.state.field1}. ${this.state.field2} встретились ${this.state.field3}. ${this.state.field4}. ${this.state.male ? "Понравилась" : "Понравился"}, отзовись ☺`;
+							let photo = this.state.imgForMessage;
+							createMessage(message,poll,photo);
+                            break;
+                        case 'sendWall':
+							this.setState({postSendId: e.detail.data.response.post_id, field1: "",
+								field2: "",	field3: "",	field4: "",	img : null,});
+                            console.log(e);
+                            break;
+                        default:
+                            console.log(e);
+                    }
+                        console.log(e);
+					break;
+				default:
+					console.log(e.detail.type);
+			}
+		});
+		connect.send('VKWebAppGetUserInfo', {});
+
+	}
+
+	componentDidUpdate() {
+		if (this.state.isReady === false && this.state.field1.length > 0 && this.state.field2.length > 0 && this.state.field3.length > 0 && this.state.field4.length > 0) {
+			this.setState({isReady: true})
+		}
+		if (this.state.isReady === true && this.state.field1.length === 0 && this.state.field2.length === 0 && this.state.field3.length === 0 && this.state.field4.length === 0) {
+			this.setState({isReady: false})
+		}
+	}
+
+	go = (e) => {
+		this.setState({ activePanel: e.currentTarget.dataset.to })
+	};
+
+
+	sendForms = () => {
+		if (!this.state.img) {
+			createPoll();
+		} else if (this.state.img) {
+			postPhotoUrl()
+		}
+	};
+
+	сhangeForms = (e) => {
+		switch (e.currentTarget.name) {
+			case 'file':
+				const file    = e.currentTarget.files[0];
+				const reader  = new FileReader();
+				reader.onloadend = () => {
+					this.setState({
+						imageUrl: reader.result
+					})
+				}
+				if (file) {
+					reader.readAsDataURL(file);
+					this.setState({
+						imageUrl :reader.result
+					})
+				}
+				else {
+					this.setState({
+						imageUrl: ""
+					})
+				}
+				this.setState({img: e.currentTarget.files[0]});
+				break;
+			default:
+				this.setState({[e.currentTarget.name]: e.currentTarget.value});
+		}
+	};
+
+	render() {
+		return (
+			<View activePanel={this.state.activePanel}>
+				<Home id="home" fetchedUser={this.state.fetchedUser} innquiryInfo={this.state} сhangeForms={this.сhangeForms} go={this.go}
+					  sendForms={this.sendForms}/>
+			</View>
+		);
+	}
+}
+
+export default App;
+
---
+++ src/panels/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Panel, Button, Group, Div,  PanelHeader, Input, FormLayout} from '@vkontakte/vkui';
+import File from "@vkontakte/vkui/dist/components/File/File";
+import img from "../img/default_img.PNG"
+
+const Home = ({ id, go, fetchedUser, ...props }) => {
+	return (
+		<Panel id={id}>
+		<PanelHeader>Найти потеряшку</PanelHeader>
+
+		<Group title="Для отправки опроса заполните пожалуйста формы:">
+			<Div>
+				<div>
+					<div>
+						<FormLayout>
+							<Input  top="Кого ищешь?" name="field1" value={props.innquiryInfo.field1} onChange={props.сhangeForms} type="text" placeholder="Кого ищешь?"/>
+							<Input  top="Время встречи" name="field2" value={props.innquiryInfo.field2}  onChange={props.сhangeForms}  type="text" placeholder="Время встречи"/>
+							<Input  top="Где встретились" name="field3"value={props.innquiryInfo.field3}   onChange={props.сhangeForms}  type="text" placeholder="Где встретились"/>
+							<Input  top="Приметы, одежда" name="field4" value={props.innquiryInfo.field4}  onChange={props.сhangeForms}  type="text" placeholder="Приметы, одежда"/>
+							<File  top="Прикрепить фото (необязательно):" onChange={props.сhangeForms} name="file"  type="file" placeholder="Прикрепить фото" accept="image/*" />
+						</FormLayout>
+					</div>
+					{props.innquiryInfo.isReady ? <Div>Ищу {props.innquiryInfo.field1}. {props.innquiryInfo.field2} встретились
+                        {props.innquiryInfo.field3}. {props.innquiryInfo.field4}. {props.innquiryInfo.male ? "Понравилась" : "Понравился"},
+                        отзовись ☺
+                        <div><img className="imgFormat" src={props.innquiryInfo.imageUrl || img} alt={"img"}/></div></Div> : <div/>}
+
+				</div>
+
+				{props.innquiryInfo.isReady ? <Button size="xl" onClick={props.sendForms}>Отправить опрос</Button> : <Button size="xl" disabled level="2">Отправить опрос</Button> }
+
+			</Div>
+		</Group>
+	</Panel>)
+};
+
+
+
+
+Home.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Home;
